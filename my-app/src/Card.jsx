6040e0af4bbcd899b11966add52ab3f99bf4d312
marvolo-riddle <iovcha@icloud.com>
2024-07-08 11:57:31 +0300

    78

---
+++ my-app/src/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    const { title, text } = this.props;
+
+    return (
+      <div className="card">
+        <div className="card-body">
+          {title ? <h4 className="card-title">{title}</h4> : null}
+          {text ? <p className="card-text">{text}</p> : null}
+          <button type="button" className="btn btn-primary">
+            Go somewhere
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
